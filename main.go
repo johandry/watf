b0b281e8f8f024b4cf396b6b6fe251ea9e0e0593
Johandry Amador <johandry@gmail.com>
2023-02-24 15:38:34 -0800

    Adding terranova into the WASM

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"log"
+	"os"
 	"strconv"
+
 	"syscall/js"
+
+	"github.com/johandry/terranova"
+	"github.com/johandry/terranova/logger"
+	"github.com/terraform-providers/terraform-provider-aws/aws"
 )
+
+var code string
+
+const stateFilename = "simple.tfstate"
 
 func add(this js.Value, i []js.Value) interface{} {
 	value1 := js.Global().Get("document").Call("getElementById", i[0].String()).Get("value").String()
@@ -49,9 +60,39 @@ func subtract(this js.Value, i []js.Value) interface{} {
 	return nil
 }
 
+func apply(this js.Value, i []js.Value) interface{} {
+	count := 0
+	keyName := "demo"
+
+	log := log.New(os.Stderr, "", log.LstdFlags)
+	logMiddleware := logger.NewMiddleware()
+	defer logMiddleware.Close()
+
+	platform, err := terranova.NewPlatform(code).
+		SetMiddleware(logMiddleware).
+		AddProvider("aws", aws.Provider()).
+		Var("c", count).
+		Var("key_name", keyName).
+		PersistStateToFile(stateFilename)
+
+	if err != nil {
+		log.Fatalf("Fail to create the platform using state file %s. %s", stateFilename, err)
+	}
+
+	terminate := (count == 0)
+	if err := platform.Apply(terminate); err != nil {
+		log.Fatalf("Fail to apply the changes to the platform. %s", err)
+	}
+
+	println("Completed")
+
+	return nil
+}
+
 func registerCallbacks() {
 	js.Global().Set("add", js.FuncOf(add))
 	js.Global().Set("subtract", js.FuncOf(subtract))
+	js.Global().Set("apply", js.FuncOf(apply))
 }
 
 func main() {
@@ -61,4 +102,20 @@ func main() {
 	// register functions
 	registerCallbacks()
 	<-c
+}
+
+func init() {
+	code = `
+  variable "c"    { default = 2 }
+  variable "key_name" {}
+  provider "aws" {
+    region        = "us-west-2"
+  }
+  resource "aws_instance" "server" {
+    instance_type = "t2.micro"
+    ami           = "ami-6e1a0117"
+    count         = "${var.c}"
+    key_name      = "${var.key_name}"
+  }
+`
 }
